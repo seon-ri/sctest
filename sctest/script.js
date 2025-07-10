f459446d886a0cfefbca0839dee6b5d7f1ba682a
seon-ri <seonresearch@gmail.com>
2025-07-10 09:32:26 +0900

    Update script.js
    주의사항 조절

--- sctest/script.js
+++ sctest/script.js
@@ -929,7 +929,8 @@ ${specialObservation.riskyItems.length > 0 ? `
       <span style="min-width:3.5em;">• <strong>${item.item}번:</strong></span>
       <div>
         ${questions[item.item - 1]}<br>
-        <span style="display:inline-block; text-indent:-1.2em; padding-left:1.2em;">→ ${getRiskyItemText(item.item)}</span>
+    <span style="display:inline-block; margin-left:1.2em;">
+      <span style="display:inline-block; min-width:1.2em;">→</span>
       </div>
     </div>
     `).join('')}
